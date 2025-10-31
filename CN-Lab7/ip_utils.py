4f23ad605fa970512890e6167458d5b4f8db82b0
Vedalaxman <151757631+Vedalaxman@users.noreply.github.com>
2025-10-31 16:08:22 +0530

    Delete CN-Lab7 directory

--- CN-Lab7/ip_utils.py
+++
@@ -1,7 +0,0 @@
-def ip_to_binary(ip_address: str) -> str:
-    octets = ip_address.split('.')
-    return ''.join(format(int(o), '08b') for o in octets)
-
-def get_network_prefix(ip_cidr: str) -> str:
-    ip, prefix = ip_cidr.split('/')
-    return ip_to_binary(ip)[:int(prefix)]
